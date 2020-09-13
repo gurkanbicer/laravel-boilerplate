41aeca8c5551799e78c3f91ff072b47f561c5195
Gürkan Biçer <gurkan@grkn.co>
2020-09-14 00:07:31 +0300

    migration files prepared

--- database/migrations/2020_09_13_000000_create_users_table.php
+++ database/migrations/2020_09_13_000000_create_users_table.php
@@ -15,11 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique()->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'superuser', 'user', 'enduser'])->default('enduser');
+            $table->enum('status', ['active', 'suspended', 'closed'])->default('active');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('display_name')->nullable();
+            $table->text('profile_image')->nullable();
+            $table->string('oauth_provider')->nullable();
+            $table->string('oauth_id')->nullable();
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }

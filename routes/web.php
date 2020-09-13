9d11c19c1ad6934f091293728156549ad3c148d1
Gürkan Biçer <gurkan@grkn.co>
2020-09-14 00:07:52 +0300

    dashboard link changed

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

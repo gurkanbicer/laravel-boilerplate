10850eb369f6ba8c2f65026eab3505782d233d46
Gürkan Biçer <gurkan@grkn.co>
2020-09-14 13:57:47 +0300

    account status check support added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -16,4 +20,48 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $account = User::query()
+            ->where('email', '=', $request->input('email'))
+            ->first();
+
+        if (!is_null($account)) {
+            if ($account->status == "suspended") {
+                $this->incrementLoginAttempts($request);
+                return $this->sendFailedLoginResponse($request, 'suspended');
+                //return Redirect::back()->with(['email', 'The account has been suspended.']);
+            } else if ($account->status == "closed") {
+                $this->incrementLoginAttempts($request);
+                return $this->sendFailedLoginResponse($request, 'closed');
+                //return Redirect::back()->with(['email', 'The account has been closed.']);
+            }
+        }
+
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    protected function sendFailedLoginResponse(Request $request, $key = 'failed')
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.' . $key)],
+        ]);
+    }
+
 }
--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,5 +15,7 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'suspended' => 'The account has been suspended.',
+    'closed' => 'The account has been closed.',
 
 ];
